a14a2c5ad545e669d53c01d4cc88d658a460f6b7
PC100 <39545635+PC100@users.noreply.github.com>
2018-10-01 11:37:30 +0530

    Update and rename pom.xml to calculate_area.php

---
+++ calculate_area.php
@@ -0,0 +1,13 @@
+#! /usr/bin/env php
+
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+  $side = isset($argv[1]) ? $argv[1] : null;
+
+  if (isset($side) && ctype_digit($side)) {
+    Shapes\Square::area($side);
+  }
+  else {
+    echo "Please provide a valid size!\n";
+  }
